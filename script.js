f44219f02100b99da7051de0101f76722f9449a8
Maham Jabbar <133046549+MahamJabbar@users.noreply.github.com>
2024-11-20 11:56:10 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+
+// 1. Variables and Data Types.......................................
+let name = "Maham";          // String
+const age = 22;             // Number (constant)
+let isEmployed = true;      // Boolean
+
+console.log('employed' , isEmployed)
+console.log(age)
+console.log(name)
+
+// 2. Basic Operators...............................................
+// let a = 29
+// let b = 20
+// let c = a + b
+// let d = a * b
+// console.log('The sum of a and b is ', c)
+// console.log('The product of a and b is ', d)
+
+// let sum = 10 + 5;           // Addition
+
+// let product = 10 * 5;       // Multiplication
+// let division = 10 / 2;      // Division
+
+// let isEqual = sum === 15;   // Comparison.....................
+
+// == equality chcek 
+// === considtion check
+
+// console.log('The sum is ', sum )
+// console.log('The Product is ',product)
+// console.log('The Division is ' , division)
+// console.log('The Equality is ', isEqual)
+
+// 3. Control Structures: Conditionals
+//  \n == Escape Sequence
+// if (isEmployed) {
+//     console.log(`${name}  is employed  \n his age is ${age}`);
+// } else {
+//     console.log(`${name} is not employed.`);
+// }
+
+
+//  4. Control Structures: Loops
+// for (let initialize, terminate, increment) {
+//     condition and output
+// } 
+    
+// for (let i = 0; i <= 50; i++) {    // for loop
+//     console.log("For Loop:", i);
+// }
+
+
+
+// let count = 0; // initialize count
+
+// while (count <= 50) {              // while loop
+//     console.log("While Loop:", count);
+//     count++; // increment
+// }
+
+
+
+// // 5. Functions
+// function greetUser(username) {
+//     return `Hello, ${username}!`;
+// }
+// console.log(greetUser(name));
